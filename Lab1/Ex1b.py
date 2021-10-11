4934d7980e024971b3c2c5356d1e9fda53bad87c
alexx-potter458 <alexx.potter458@gmail.com>
2021-10-12 00:11:18 +0300

    ex1

--- Lab1/Ex1b.py
+++ Lab1/Ex1b.py
@@ -1,13 +1,3 @@
-#import matplotlib.pyplot as plt
-#import numpy as np
-
-#def f(x): return x**6-x-1
-#interval = np.linspace(-2,2)
-#y=f(interval)
-#plt.plot(interval,y,c='purple',linewidth=3)
-#plt.axhline(0)
-     
-#%%
 import matplotlib.pyplot as plt
 import numpy as np 
 
