c86552c3348bcc76c4533374f8a5abe62c8f8227
alexx-potter458 <alexx.potter458@gmail.com>
2021-12-02 14:04:34 +0200

    gg

--- Lab8/Ex3a.py
+++ Lab8/Ex3a.py
@@ -1,2 +1,51 @@
+import numpy as np
+
 def SplineCubic(f,a,b,n,x):
-    return 0
+
+    xDivs = np.linspace(a,b,n+1)
+    yDivs=f(xDivs)
+    n = n+1
+
+    xdiff = np.diff(xDivs)
+    ydiff = np.diff(yDivs)
+
+    Li = np.zeros(n)
+    Li_1 = np.zeros(n-1)
+    zDivs = np.zeros(n)
+
+    Li[0] = xdiff[0]**(1/2)
+    Li_1[0] = 0
+    zDivs[0] = 0
+
+    for i in range(1, n-1, 1):
+        Li_1[i] = xdiff[i-1] / Li[i-1]
+        Li[i] = (2 * (xdiff[i - 1] + xdiff[i]) - Li_1[i - 1] * Li_1[i - 1])**(1/2)
+        
+        Bi = 6 * (ydiff[i] / xdiff[i] - ydiff[i - 1] / xdiff[i - 1])
+        zDivs[i] = (Bi - Li_1[i - 1] * zDivs[i - 1]) / Li[i]
+
+    i = n - 1
+    
+    Li_1[i - 1] = xdiff[-1] / Li[i - 1]
+    Li[i] = (2 * xdiff[-1] - Li_1[i - 1] * Li_1[i - 1])**(1/2)
+    
+    Bi = 0
+    
+    zDivs[i] = (Bi - Li_1[i - 1] * zDivs[i - 1]) / Li[i]
+
+    i = n - 1
+    zDivs[i] = zDivs[i] / Li[i]
+    
+    for i in range(n-2, -1, -1):
+        zDivs[i] = (zDivs[i] - Li_1[i - 1] * zDivs[i + 1]) / Li[i]
+
+    i = xDivs.searchsorted(x)
+
+    xi1, xi0 = xDivs[i], xDivs[i - 1]
+    yi1, yi0 = yDivs[i], yDivs[i - 1]
+    zi1, zi0 = zDivs[i], zDivs[i - 1]
+    hi1 = xi1 - xi0
+
+    y = zi0 / (6 * hi1) * (xi1 - x)**3 + zi1 / (6 * hi1)*(x - xi0)**3 + (yi1 / hi1 - zi1 * hi1 / 6) * (x - xi0) + (yi0 / hi1 - zi0 * hi1 / 6) * (xi1 - x)
+    
+    return y
---
+++ Lab8/Ex3b.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Ex3a import SplineCubic
+
+def f(x): return np.exp(2*x)
+a = -1
+b = 1
+n = 5
+x = np.linspace(a, b)
+Sn = []
+
+for xi in x:
+    Sn.append(SplineCubic(f, a, b, n, xi))
+
+plt.title('Spline cubic')
+plt.plot(x, f(x))
+plt.plot(x, Sn, linestyle="--")
+
+plt.figure()
+
+plt.title('Eroare spline cubic')
+def errAbs(y, yp): return abs(y-yp)
+errs = errAbs(f(x), Sn)
+plt.plot(x, errs)
+
+plt.show()
